cfd314657382a29c091b7698849f9a4951c0e42c
SkuldNorniern <skuldnorniern@gmail.com>
2024-09-24 09:35:39 +0900

    feat: add support for code highlight

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,13 @@ export default defineNuxtConfig({
       enabled: true
     }
   },
-
-  modules: ["@nuxt/content"]
+  content: {
+    // Other Nuxt Content configurations...
+    highlight: {
+      theme: 'github-dark', // or any other Shiki theme you prefer
+      // Optionally, specify additional languages you need highlighting for
+      langs: ['rust', 'python', 'c', 'cpp']
+    }
+  },
+ modules: ['@nuxt/content']
 })
